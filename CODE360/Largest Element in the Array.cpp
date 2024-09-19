e933d91976078dcfbe2d1040d4b8e15d18f21fee
PRIYESH SINGH <146026588+PRIYESHSINGH24@users.noreply.github.com>
2024-09-19 18:57:43 +0530

    Update Largest Element in the Array.cpp
    code added in the file

--- CODE360/Largest Element in the Array.cpp
+++ CODE360/Largest Element in the Array.cpp
@@ -1,1 +1,15 @@
 
+int largestElement(vector<int> &arr, int n) {
+    if(n == 0) {
+        cout << "Array is empty" << endl;
+        return -1;
+    }
+    
+    int largest = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
